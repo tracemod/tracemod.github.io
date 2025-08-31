33e2386914e258bfa6b625547a43cff5ce421ae2
tracemod <43665428+tracemod@users.noreply.github.com>
2025-08-31 17:48:42 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,18 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
 	var mySrc = myImage.getAttribute('src');
-	if (mySrc === 'images/just5_logo_orange_rgb.jpg')
-		myImage.setAttribute('src', 'images/tabby-cat-sleeping-bob-gibbonsscience-photo-library.jpg');
-	else
+	if (mySrc === 'images/fire.jpg')
 		myImage.setAttribute('src', 'images/just5_logo_orange_rgb.jpg');
+	else if (mySrc === 'images/just5_logo_orange_rgb.jpg')
+		myImage.setAttribute('src', 'images/tabby-cat-sleeping-bob-gibbonsscience-photo-library.jpg');
+	else if (mySrc === 'images/tabby-cat-sleeping-bob-gibbonsscience-photo-library.jpg')
+		myImage.setAttribute('src', 'images/landscape.jpg');
+	else if (mySrc === 'images/landscape.jpg')
+		myImage.setAttribute('src', 'images/medusa.jpg');
+	else if (mySrc === 'images/medusa.jpg')
+		myImage.setAttribute('src', 'images/todd.jpg');	
+	else
+		myImage.setAttribute('src', 'images/fire.jpg');	
 }
 
 var myButton = document.querySelector('button');
@@ -13,8 +21,12 @@ var myHeading = document.querySelector('h1');
 
 function setUserName() {
 	var myName = prompt('Please, enter your name');
-	localStorage.setItem('name', myName);
-	myHeading.innerHTML = 'Hello, ' + myName;
+	if (myName == '')	
+		myHeading.innerHTML = 'Hello!';
+	else {
+		localStorage.setItem('name', myName);
+		myHeading.innerHTML = 'Hello, ' + myName + ' !';
+	}
 }
 
 if (!localStorage.getItem('name'))
